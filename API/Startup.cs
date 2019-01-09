51a0957f217efea48ec521448748401f3e43a406
Benjamen Alford <benjamen.alford@gmail.com>
2019-01-08 18:30:14 -0600

    setup basic DB and swagger UI

--- API/Startup.cs
+++ API/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace API
 {
@@ -25,6 +26,12 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddSwaggerGen(c =>
+              {
+                  c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+              });
+
             services.AddCors();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
@@ -35,13 +42,26 @@ namespace API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                // Enable middleware to serve generated Swagger as a JSON endpoint.
+                app.UseSwagger();
+                // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+                // specifying the Swagger JSON endpoint.
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                    c.RoutePrefix = string.Empty;
+                });
+
             }
             else
             {
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+
+            //turn off https for now
+            // app.UseHttpsRedirection();
+
             app.UseMvc();
         }
     }
